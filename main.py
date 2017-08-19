f8a1dd918ade054d138e6858e063870f7fd0e1cc
Brad_Hester <hesterbj@iinet.net.au>
2017-08-19 11:49:26 +1000

    Reformatting

--- main.py
+++ main.py
@@ -6,24 +6,24 @@ import LightUtility
 import time
 import geolocation
 
-#main
+# main
 
-TimeNow=geolocation.GetTimeNow()
+TimeNow = geolocation.GetTimeNow()
 
 geolocation.geocode("3 Galileo Street Gregory Hills")
 
-if xmlUtility.GetfromXML("Lights","overrideon")=="True":
+if xmlUtility.GetfromXML("Lights", "overrideon") == "True":
     print("Override On")
-    z=time.strptime(xmlUtility.GetfromXML("Lights","OverrideTime"),"%H:%M")
-    y= TimeNow.replace(hour=z.tm_hour, minute=z.tm_min, second=0, microsecond=0)
+    z = time.strptime(xmlUtility.GetfromXML("Lights", "OverrideTime"), "%H:%M")
+    y = TimeNow.replace(hour=z.tm_hour, minute=z.tm_min, second=0, microsecond=0)
     if TimeNow > y and TimeNow < LightUtility.GetTimeOff():
         print("Lights should be on")
-        if LightUtility.AreLightsOn()==0:
+        if LightUtility.AreLightsOn() == 0:
             LightUtility.TurnLightsOn()
         else:
             print("Lights already Running")
     else:
-        if LightUtility.AreLightsOn()==1:
+        if LightUtility.AreLightsOn() == 1:
             LightUtility.TurnLightsOff()
         else:
             print("Nothing to do here")
